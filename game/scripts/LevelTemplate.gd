b6acdc2aabf023ebf48a039fef00ae7ef3185a48
Brett Haines <bhaines3@gatech.edu>
2019-12-11 16:51:10 -0500

    Hooking timer signals into level

--- game/scripts/LevelTemplate.gd
+++ game/scripts/LevelTemplate.gd
@@ -4,9 +4,17 @@ onready var Echo = preload("res://scenes/Echo.tscn")
 
 func _ready():
 	$Character.connect("noise_made", self, "_on_noise_made")
+	GameTimer.connect("noise_made", self, "_on_noise_made")
+	GameTimer.connect("hour_elapsed", self, "_on_hour_elapsed")
+	
+	GameTimer.start_new_game()
 
 func _on_noise_made(echo_scale, location):
 	var new_echo = Echo.instance()
 	add_child(new_echo)
 	new_echo.position = location
 	new_echo.trigger_echo(echo_scale)
+
+func _on_hour_elapsed(hours_left):
+	# TODO: Create popup telling players number of hours to midnight
+	pass
