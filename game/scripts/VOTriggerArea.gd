f3193eb42cb41fb57759f7898268dc1d289224df
Brett Haines <bhaines418@gmail.com>
2019-12-15 13:31:27 -0800

    Reorganizing VO files for clarity

--- game/scripts/VOTriggerArea.gd
+++ game/scripts/VOTriggerArea.gd
@@ -1,5 +1,4 @@
 extends Area2D
-class_name VOTriggerArea
 
 export var vo_line_num : int = 0
 var already_triggered : bool = false
@@ -18,49 +17,42 @@ func _on_body_entered(body):
 		
 		match vo_line_num:
 			1:
-				play_line(0)
-				yield(audio_player, "finished")
-				yield(get_tree().create_timer(0.6), "timeout")
 				play_line(1)
 				yield(audio_player, "finished")
 				queue_free()
 			2:
-				play_line(3)
+				play_line(2)
 				yield(audio_player, "finished")
 				yield(get_tree().create_timer(1.0), "timeout")
-				play_line(4)
+				play_line(3)
 				yield(audio_player, "finished")
 				yield(get_tree().create_timer(2.5), "timeout")
-				play_line(5)
+				play_line(4)
 				yield(audio_player, "finished")
 				queue_free()
 			3:
-				play_line(6)
-				yield(audio_player, "finished")
-				play_line(7)
+				play_line(5)
 				yield(audio_player, "finished")
 				queue_free()
 			4:
-				play_line(8)
-				yield(audio_player, "finished")
-				play_line(9)
+				play_line(6)
 				yield(audio_player, "finished")
 				queue_free()
 			5:
 				if body.has_level_end_key:
-					play_line(10)
+					play_line(7)
 					yield(audio_player, "finished")
 					queue_free()
 			6:
-				play_line(11)
+				play_line(8)
 				yield(audio_player, "finished")
 				# TODO: Player steps back
-				play_line(12)
+				play_line(9)
 				yield(audio_player, "finished")
 				queue_free()
 
 func play_line(line_num : int):
-	var line_file = "res://assets/sounds/vo_line_%02d.ogg" % (line_num+1)
+	var line_file = "res://assets/sounds/vo_line_%02d.ogg" % line_num
 	var line = load(line_file)
 	audio_player.stream = line
 	audio_player.play()
