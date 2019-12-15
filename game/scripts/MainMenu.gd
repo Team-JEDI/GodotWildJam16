77d7cfb78a9d67fea4b2ee3f5a1fbd56c4e8a241
John Gabriel <contact@johngabriel.co.uk>
2019-12-15 23:31:27 +0000

    Add new title screen & implement credits

--- game/scripts/MainMenu.gd
+++ game/scripts/MainMenu.gd
@@ -1,10 +1,30 @@
 extends Control
 
+onready var button_load = $Menu/VBoxContainer/LoadBtn
+onready var menu = $Menu
+onready var credits = $Credits
+
+onready var music = $AudioStreamPlayer_Music
+onready var ambience = $AudioStreamPlayer_Ambience
+onready var video_player = $VideoPlayer
+onready var blackout = $Blackout
+onready var tween = $Tween
+
 func _ready():
-	
+	video_player.play()
 	# Only show "LOAD" btn if there's a game to load
 	if not File.new().file_exists(LoadHelper.SAVE_FILEPATH):
-		$VBoxContainer/LoadBtn.disabled = true
+		button_load.disabled = true
+	# Set up music/ambience/fade-in
+	ambience.volume_db = -40
+	tween.interpolate_property(ambience, "volume_db", -40, -15, 5.0, Tween.TRANS_LINEAR, Tween.EASE_IN, 2.0)
+	tween.interpolate_property(blackout, "color", Color.black, Color(0.0, 0.0, 0.0, 0.0), 1.0, Tween.TRANS_LINEAR, Tween.EASE_OUT, 1.0)
+	tween.start()
+	ambience.play()
+	yield(get_tree().create_timer(1), "timeout")
+	music.play()
+	yield(get_tree().create_timer(1), "timeout")
+	blackout.hide()
 
 func _on_load_game():
 	LoadHelper.is_loading = true
@@ -16,8 +36,15 @@ func _on_new_game():
 	get_tree().change_scene("res://scenes/levels/1.tscn")
 
 func _on_view_credits():
-	# TODO: Create credits page
-	pass
+	menu.hide()
+	credits.show()
+
+func _on_BackBtn_pressed():
+	menu.show()
+	credits.hide()
 
 func _on_quit_game():
 	get_tree().quit()
+
+func _on_VideoPlayer_finished():
+	video_player.play()
