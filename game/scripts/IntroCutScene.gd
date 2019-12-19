5c0eea6e7506972fcd82402a80de3275b2117a2f
Brett Haines <bhaines3@gatech.edu>
2019-12-19 16:31:09 -0500

    Adding option to skip intro cutscene

--- game/scripts/IntroCutScene.gd
+++ game/scripts/IntroCutScene.gd
@@ -6,5 +6,9 @@ func _ready():
 	player.connect("finished", self, "_on_video_finished")
 	player.play()
 
+func _process(delta):
+	if Input.is_key_pressed(KEY_SPACE) or Input.is_key_pressed(KEY_ENTER) or Input.is_mouse_button_pressed(BUTTON_LEFT):	
+		get_tree().change_scene("res://scenes/levels/1.tscn")
+
 func _on_video_finished():
 	get_tree().change_scene("res://scenes/levels/1.tscn")
